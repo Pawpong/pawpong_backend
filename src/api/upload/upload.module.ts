f14008f1e654857604886d81500454955622ea9e
rtaeho <ryou0920@naver.com>
2025-10-07 17:58:14 +0900

    feat: #24 브리더 파일 업로드 로직 수정

--- src/api/upload/upload.module.ts
+++ src/api/upload/upload.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UploadController } from './upload.controller';
 import { StorageModule } from '../../common/storage/storage.module';
+import { BreederDatabaseModule, AdopterDatabaseModule } from '../../common/database/database.module';
 
 @Module({
-  imports: [StorageModule],
+  imports: [
+    StorageModule,
+    BreederDatabaseModule,
+    AdopterDatabaseModule,
+  ],
   controllers: [UploadController],
 })
 export class UploadModule {}
