7297aca90131f7bd0714a1dac063467eee8f6e0e
KIM SEUNG CHAN <201921339@sangmyung.kr>
2025-10-21 00:44:59 +0900

    feat: 파일 업로드 API에 Response Type 정의 추가
    - 다중 파일 업로드 Response DTO 생성
    - 파일 삭제 Response DTO 생성
    - 대표 사진, 반려동물 사진 업로드 API에 responseType 추가

--- src/api/upload/upload.module.ts
+++ src/api/upload/upload.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { UploadController } from './upload.controller';
 import { StorageModule } from '../../common/storage/storage.module';
 import { BreederDatabaseModule, AdopterDatabaseModule } from '../../common/database/database.module';
+import { CustomLoggerService } from '../../common/logger/custom-logger.service';
 
 @Module({
-  imports: [
-    StorageModule,
-    BreederDatabaseModule,
-    AdopterDatabaseModule,
-  ],
-  controllers: [UploadController],
+    imports: [StorageModule, BreederDatabaseModule, AdopterDatabaseModule],
+    controllers: [UploadController],
+    providers: [CustomLoggerService],
 })
 export class UploadModule {}
