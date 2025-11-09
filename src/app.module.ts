99fd98a9386606132a6bf873651fc92418ae7c0e
KIM SEUNG CHAN <201921339@sangmyung.kr>
2025-11-09 17:15:02 +0900

    chore: 코드 품질 개선

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,14 +8,16 @@ import { CustomLoggerService } from './common/logger/custom-logger.service';
 import { DatabaseModule } from './common/database/database.module';
 import { AuthModule } from './api/auth/auth.module';
 import { HomeModule } from './api/home/home.module';
-import { BreederModule } from './api/breeder/breeder.module';
-import { AdopterModule } from './api/adopter/adopter.module';
-import { BreederManagementModule } from './api/breeder-management/breeder-management.module';
-import { AdminModule } from './api/admin/admin.module';
+import { BreedModule } from './api/breed/breed.module';
 import { HealthModule } from './api/health/health.module';
 import { UploadModule } from './api/upload/upload.module';
+import { BreederModule } from './api/breeder/breeder.module';
+import { AdopterModule } from './api/adopter/adopter.module';
 import { DistrictModule } from './api/district/district.module';
-import { BreedModule } from './api/breed/breed.module';
+import { UserAdminModule } from './api/user/admin/user-admin.module';
+import { PlatformAdminModule } from './api/platform/admin/platform-admin.module';
+import { StandardQuestionModule } from './api/standard-question/standard-question.module';
+import { BreederManagementModule } from './api/breeder-management/breeder-management.module';
 
 @Module({
     imports: [
@@ -25,12 +27,14 @@ import { BreedModule } from './api/breed/breed.module';
         }),
         WinstonModule.forRoot(winstonConfig),
         DatabaseModule,
+        StandardQuestionModule,
         AuthModule,
         HomeModule,
         BreederModule,
         AdopterModule,
         BreederManagementModule,
-        AdminModule,
+        UserAdminModule,
+        PlatformAdminModule,
         HealthModule,
         UploadModule,
         DistrictModule,
