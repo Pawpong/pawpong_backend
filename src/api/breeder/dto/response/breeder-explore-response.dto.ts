f8b1c310324d2258294783e696742848ce981bf9
KIM SEUNG CHAN <201921339@sangmyung.kr>
2025-11-16 16:39:02 +0900

    fix: import 수정

--- src/api/breeder/dto/response/breeder-explore-response.dto.ts
+++ src/api/breeder/dto/response/breeder-explore-response.dto.ts
@@ -1,4 +1,4 @@
-import { PaginationResponseDto } from '../../../common/dto/pagination/pagination-response.dto';
+import { PaginationResponseDto } from '../../../../common/dto/pagination/pagination-response.dto';
 import { BreederCardResponseDto } from './breeder-card-response.dto';
 
 export class BreederExploreResponseDto extends PaginationResponseDto<BreederCardResponseDto> {}
