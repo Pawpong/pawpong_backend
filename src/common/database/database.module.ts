56f0584fede1dad83c1f5d1ba7b7ed2166c92338
rtaeho <ryou0920@naver.com>
2025-12-25 13:07:37 +0900

    Revert "chore: 환경변수 설정 변경"
    This reverts commit 102c6827ce1b76cd72249af9ab72d804da25272b.

--- src/common/database/database.module.ts
+++ src/common/database/database.module.ts
@@ -6,25 +6,8 @@ import { ConfigService } from '@nestjs/config';
     imports: [
         MongooseModule.forRootAsync({
             useFactory: async (configService: ConfigService) => {
-                // APP_ENV에 따라 MongoDB URI 선택
-                const appEnv = configService.get<string>('APP_ENV') || 'local';
-                let uri: string | undefined;
-
-                if (appEnv === 'local') {
-                    uri = configService.get<string>('MONGODB_URI_LOCAL');
-                } else if (appEnv === 'dev') {
-                    uri = configService.get<string>('MONGODB_URI_DEV');
-                } else {
-                    // prod
-                    uri = configService.get<string>('MONGODB_URI_PROD');
-                }
-
-                if (!uri) {
-                    throw new Error(`MongoDB URI not found for environment: ${appEnv}`);
-                }
-
-                console.log('[DatabaseModule] APP_ENV:', appEnv);
-                console.log('[DatabaseModule] MongoDB URI:', uri.replace(/:[^:]*@/, ':****@'));
+                const uri = configService.get<string>('MONGODB_URI');
+                console.log('[DatabaseModule] MongoDB URI:', uri ? uri.replace(/:[^:]*@/, ':****@') : 'NOT SET');
                 console.log('[DatabaseModule] Connecting to MongoDB...');
 
                 return {
