5b35796a1d28c80ae5b92348f7cd8cece11b7fe4
KIM SEUNG CHAN <201921339@sangmyung.kr>
2026-04-07 16:44:50 +0900

    refactor(app-version-admin): 조회와 명령 컨트롤러를 분리

---
+++ src/api/app-version/admin/app-version-admin-query.controller.ts
@@ -0,0 +1,23 @@
+import { Get, Query } from '@nestjs/common';
+
+import { PaginationRequestDto } from '../../../common/dto/pagination/pagination-request.dto';
+import { PaginationResponseDto } from '../../../common/dto/pagination/pagination-response.dto';
+import { ApiResponseDto } from '../../../common/dto/response/api-response.dto';
+import { GetAppVersionListUseCase } from './application/use-cases/get-app-version-list.use-case';
+import { AppVersionAdminProtectedController } from './decorator/app-version-admin-controller.decorator';
+import { AppVersionResponseDto } from '../dto/response/app-version-response.dto';
+import { ApiGetAppVersionListAdminEndpoint } from './swagger';
+
+@AppVersionAdminProtectedController()
+export class AppVersionAdminQueryController {
+    constructor(private readonly getAppVersionListUseCase: GetAppVersionListUseCase) {}
+
+    @Get()
+    @ApiGetAppVersionListAdminEndpoint()
+    async getAppVersionList(
+        @Query() paginationData: PaginationRequestDto,
+    ): Promise<ApiResponseDto<PaginationResponseDto<AppVersionResponseDto>>> {
+        const result = await this.getAppVersionListUseCase.execute(paginationData);
+        return ApiResponseDto.success(result, '앱 버전 목록 조회 성공');
+    }
+}
