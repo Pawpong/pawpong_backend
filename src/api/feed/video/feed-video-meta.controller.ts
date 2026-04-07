3b4fe21f21677ca7a88fbb5132428cfe03254f64
KIM SEUNG CHAN <201921339@sangmyung.kr>
2026-04-07 17:18:16 +0900

    refactor(feed-video): 공개 피드와 스트리밍 컨트롤러를 역할별로 분리

--- src/api/feed/video/feed-video-meta.controller.ts
+++ src/api/feed/video/feed-video-meta.controller.ts
@@ -1,44 +1,21 @@
-import { Get, Param, Post, Query } from '@nestjs/common';
+import { Get, Param, Post } from '@nestjs/common';
 
-import { GetFeedUseCase } from './application/use-cases/get-feed.use-case';
-import { GetPopularVideosUseCase } from './application/use-cases/get-popular-videos.use-case';
 import { GetVideoMetaUseCase } from './application/use-cases/get-video-meta.use-case';
 import { IncrementViewCountUseCase } from './application/use-cases/increment-view-count.use-case';
 import { FeedPublicController } from './decorator/feed-video-controller.decorator';
 import {
-    FeedResponseDto,
     PendingVideoMetaResponseDto,
-    PopularVideoItemDto,
     VideoActionSuccessResponseDto,
     VideoMetaResponseDto,
 } from './dto/response/video-response.dto';
-import {
-    ApiGetFeedVideoMetaEndpoint,
-    ApiGetFeedVideosEndpoint,
-    ApiGetPopularFeedVideosEndpoint,
-    ApiIncrementFeedVideoViewEndpoint,
-} from './swagger';
+import { ApiGetFeedVideoMetaEndpoint, ApiIncrementFeedVideoViewEndpoint } from './swagger';
 
 @FeedPublicController()
-export class FeedVideoPublicController {
+export class FeedVideoMetaController {
     constructor(
-        private readonly getFeedUseCase: GetFeedUseCase,
-        private readonly getPopularVideosUseCase: GetPopularVideosUseCase,
         private readonly getVideoMetaUseCase: GetVideoMetaUseCase,
         private readonly incrementViewCountUseCase: IncrementViewCountUseCase,
     ) {}
-
-    @Get('videos')
-    @ApiGetFeedVideosEndpoint()
-    async getFeed(@Query('page') page: number = 1, @Query('limit') limit: number = 20): Promise<FeedResponseDto> {
-        return this.getFeedUseCase.execute(Number(page), Number(limit));
-    }
-
-    @Get('videos/popular')
-    @ApiGetPopularFeedVideosEndpoint()
-    async getPopularVideos(@Query('limit') limit: number = 10): Promise<PopularVideoItemDto[]> {
-        return this.getPopularVideosUseCase.execute(Number(limit));
-    }
 
     @Get('videos/:videoId')
     @ApiGetFeedVideoMetaEndpoint()
--- src/api/feed/video/feed-video-prefetch.controller.ts
+++ src/api/feed/video/feed-video-prefetch.controller.ts
@@ -1,24 +1,17 @@
-import { Get, Param, Post, Query, Res } from '@nestjs/common';
-import type { Response } from 'express';
+import { Param, Post, Query } from '@nestjs/common';
 
 import { PrefetchAllQualitySegmentsUseCase } from './application/use-cases/prefetch-all-quality-segments.use-case';
 import { FeedPublicController } from './decorator/feed-video-controller.decorator';
 import { SegmentPrefetchResponseDto } from './dto/response/video-response.dto';
 import { FeedVideoStreamHttpService } from './infrastructure/feed-video-stream-http.service';
-import { ApiPrefetchFeedVideoSegmentsEndpoint, ApiStreamFeedVideoEndpoint } from './swagger';
+import { ApiPrefetchFeedVideoSegmentsEndpoint } from './swagger';
 
 @FeedPublicController()
-export class FeedVideoStreamController {
+export class FeedVideoPrefetchController {
     constructor(
         private readonly prefetchAllQualitySegmentsUseCase: PrefetchAllQualitySegmentsUseCase,
         private readonly feedVideoStreamHttpService: FeedVideoStreamHttpService,
     ) {}
-
-    @Get('videos/stream/:videoId/:filename')
-    @ApiStreamFeedVideoEndpoint()
-    async streamHLS(@Param('videoId') videoId: string, @Param('filename') filename: string, @Res() res: Response) {
-        return this.feedVideoStreamHttpService.stream(videoId, filename, res);
-    }
 
     @Post('videos/stream/:videoId/prefetch')
     @ApiPrefetchFeedVideoSegmentsEndpoint()
