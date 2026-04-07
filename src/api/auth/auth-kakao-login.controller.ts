f71931d56c39cb296f269789b04295790c2bd8dd
KIM SEUNG CHAN <201921339@sangmyung.kr>
2026-04-07 17:18:06 +0900

    refactor(auth): 소셜 로그인 컨트롤러를 공급자별로 분리

---
+++ src/api/auth/auth-kakao-login.controller.ts
@@ -0,0 +1,32 @@
+import { Get, Req, Res, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import type { Response } from 'express';
+
+import { AuthPublicController } from './decorator/auth-public-controller.decorator';
+import { AuthSocialHttpFlowService } from './domain/services/auth-social-http-flow.service';
+import { ApiKakaoCallbackEndpoint, ApiKakaoLoginEndpoint } from './swagger';
+
+@AuthPublicController()
+export class AuthKakaoLoginController {
+    constructor(private readonly authSocialHttpFlowService: AuthSocialHttpFlowService) {}
+
+    @Get('kakao')
+    @ApiKakaoLoginEndpoint()
+    kakaoLogin(@Req() req, @Res() res: Response) {
+        return res.redirect(
+            this.authSocialHttpFlowService.getRedirectUrl(
+                'kakao',
+                req.headers.referer,
+                req.headers.origin,
+                req.query.returnUrl as string | undefined,
+            ),
+        );
+    }
+
+    @Get('kakao/callback')
+    @UseGuards(AuthGuard('kakao'))
+    @ApiKakaoCallbackEndpoint()
+    async kakaoCallback(@Req() req, @Res() res: Response) {
+        return this.authSocialHttpFlowService.handleCallback(req.user, res);
+    }
+}
