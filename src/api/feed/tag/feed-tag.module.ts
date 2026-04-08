b6d1ce6805714016fc2376f7ebf28e67d16edc0c
KIM SEUNG CHAN <201921339@sangmyung.kr>
2026-04-08 15:25:37 +0900

    refactor(feed-tag): 스토리지 signed url 경계를 포트로 분리

--- src/api/feed/tag/feed-tag.module.ts
+++ src/api/feed/tag/feed-tag.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Video, VideoSchema } from '../../../schema/video.schema';
 import { StorageModule } from '../../../common/storage/storage.module';
+import { FEED_TAG_ASSET_URL } from './application/ports/feed-tag-asset-url.port';
 import { SearchByTagUseCase } from './application/use-cases/search-by-tag.use-case';
 import { GetPopularTagsUseCase } from './application/use-cases/get-popular-tags.use-case';
 import { SuggestTagsUseCase } from './application/use-cases/suggest-tags.use-case';
 import { FeedTagQueryService } from './domain/services/feed-tag-query.service';
 import { FeedTagPresentationService } from './domain/services/feed-tag-presentation.service';
 import { FeedTagMongooseReaderAdapter } from './infrastructure/feed-tag-mongoose-reader.adapter';
+import { FeedTagStorageAssetUrlAdapter } from './infrastructure/feed-tag-storage-asset-url.adapter';
 import { FeedTagRepository } from './repository/feed-tag.repository';
 import { FEED_TAG_READER } from './application/ports/feed-tag-reader.port';
 
@@ -27,9 +29,14 @@ import { FEED_TAG_READER } from './application/ports/feed-tag-reader.port';
         FeedTagPresentationService,
         FeedTagRepository,
         FeedTagMongooseReaderAdapter,
+        FeedTagStorageAssetUrlAdapter,
         {
             provide: FEED_TAG_READER,
             useExisting: FeedTagMongooseReaderAdapter,
+        },
+        {
+            provide: FEED_TAG_ASSET_URL,
+            useExisting: FeedTagStorageAssetUrlAdapter,
         },
     ],
     exports: [SearchByTagUseCase, GetPopularTagsUseCase, SuggestTagsUseCase],
