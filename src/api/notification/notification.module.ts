efc413e95df04d03b70389445265a83c05ff046c
KIM SEUNG CHAN <201921339@sangmyung.kr>
2026-04-08 15:54:34 +0900

    refactor(notification): 메일 모듈 경계를 명시적으로 정리

--- src/api/notification/notification.module.ts
+++ src/api/notification/notification.module.ts
@@ -35,7 +35,6 @@ import { MailModule } from '../../common/mail/mail.module';
  * 알림 모듈
  *
  * 서비스 알림 및 이메일 발송 기능을 통합 제공합니다.
- * MailModule을 re-export하여 다른 모듈이 MailService와 MailTemplateService를 사용할 수 있도록 합니다.
  */
 @Module({
     imports: [MongooseModule.forFeature([{ name: Notification.name, schema: NotificationSchema }]), MailModule],
@@ -80,6 +79,6 @@ import { MailModule } from '../../common/mail/mail.module';
             useExisting: NotificationDispatchService,
         },
     ],
-    exports: [NotificationDispatchPort, MailModule], // MailModule re-export
+    exports: [NotificationDispatchPort],
 })
 export class NotificationModule {}
