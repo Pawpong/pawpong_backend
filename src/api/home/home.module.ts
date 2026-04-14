433b3e6a8c295ce65fbc18ee4aebf632f171723d
KIM SEUNG CHAN <201921339@sangmyung.kr>
2026-04-14 16:16:50 +0900

    refactor(home): 모듈 정의를 역할별 배열로 분리
    - home module의 schema import와 public controller 구성을 definition 파일로 이동해 선언부를 얇게 정리함
    - use-case, domain, infrastructure, port binding provider를 역할 기준 배열로 나눠 홈 조회 구성을 읽기 쉽게 정리함

--- src/api/home/home.module.ts
+++ src/api/home/home.module.ts
@@ -1,61 +1,14 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 
-import { GetActiveBannersUseCase } from './application/use-cases/get-active-banners.use-case';
-import { GetAvailablePetsUseCase } from './application/use-cases/get-available-pets.use-case';
-import { GetFaqsUseCase } from './application/use-cases/get-faqs.use-case';
-import { HomeAvailablePetsController } from './home-available-pets.controller';
-import { HomeBannersController } from './home-banners.controller';
-import { HomeFaqsController } from './home-faqs.controller';
-import { HomeAvailablePetCatalogService } from './domain/services/home-available-pet-catalog.service';
-import { HomeBannerCatalogService } from './domain/services/home-banner-catalog.service';
-import { HomeFaqCatalogService } from './domain/services/home-faq-catalog.service';
-import { HOME_ASSET_URL_PORT } from './application/ports/home-asset-url.port';
-import { HOME_CONTENT_READER_PORT } from './application/ports/home-content-reader.port';
-import { HomeMongooseContentReaderAdapter } from './infrastructure/home-mongoose-content-reader.adapter';
-import { HomeStorageAssetUrlAdapter } from './infrastructure/home-storage-asset-url.adapter';
-import { AvailablePetRepository } from './repository/available-pet.repository';
-import { BannerRepository } from './repository/banner.repository';
-import { FaqRepository } from './repository/faq.repository';
-
-import { Faq, FaqSchema } from '../../schema/faq.schema';
-import { Banner, BannerSchema } from '../../schema/banner.schema';
-import { Breeder, BreederSchema } from '../../schema/breeder.schema';
-import { AvailablePet, AvailablePetSchema } from '../../schema/available-pet.schema';
-
-import { StorageModule } from '../../common/storage/storage.module';
+import {
+    HOME_MODULE_CONTROLLERS,
+    HOME_MODULE_IMPORTS,
+    HOME_MODULE_PROVIDERS,
+} from './home.module-definition';
 
 @Module({
-    imports: [
-        MongooseModule.forFeature([
-            { name: Banner.name, schema: BannerSchema },
-            { name: Faq.name, schema: FaqSchema },
-            { name: Breeder.name, schema: BreederSchema },
-            { name: AvailablePet.name, schema: AvailablePetSchema },
-        ]),
-        StorageModule,
-    ],
-    controllers: [HomeBannersController, HomeFaqsController, HomeAvailablePetsController],
-    providers: [
-        GetActiveBannersUseCase,
-        GetFaqsUseCase,
-        GetAvailablePetsUseCase,
-        HomeBannerCatalogService,
-        HomeFaqCatalogService,
-        HomeAvailablePetCatalogService,
-        AvailablePetRepository,
-        BannerRepository,
-        FaqRepository,
-        HomeMongooseContentReaderAdapter,
-        HomeStorageAssetUrlAdapter,
-        {
-            provide: HOME_CONTENT_READER_PORT,
-            useExisting: HomeMongooseContentReaderAdapter,
-        },
-        {
-            provide: HOME_ASSET_URL_PORT,
-            useExisting: HomeStorageAssetUrlAdapter,
-        },
-    ],
+    imports: HOME_MODULE_IMPORTS,
+    controllers: HOME_MODULE_CONTROLLERS,
+    providers: HOME_MODULE_PROVIDERS,
 })
 export class HomeModule {}
