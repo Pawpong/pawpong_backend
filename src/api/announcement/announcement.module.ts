f2847c0263494d180ff784a5340e880319d56f89
KIM SEUNG CHAN <201921339@sangmyung.kr>
2026-04-14 15:17:52 +0900

    refactor(announcement): 모듈 정의를 역할별 배열로 분리
    - announcement module의 public/admin controller 구성을 definition 파일로 이동해 선언부를 얇게 정리함
    - use-case, domain, infrastructure, port binding provider를 역할 기준 배열로 나눠 공지 기능 구성을 읽기 쉽게 정리함

--- src/api/announcement/announcement.module.ts
+++ src/api/announcement/announcement.module.ts
@@ -1,64 +1,18 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 
-import { AnnouncementController } from './announcement.controller';
-import { AnnouncementAdminCommandController } from './admin/announcement-admin-command.controller';
-import { AnnouncementAdminQueryController } from './admin/announcement-admin-query.controller';
-
-import { CustomLoggerService } from '../../common/logger/custom-logger.service';
-import { ANNOUNCEMENT_PUBLIC_READER_PORT } from './application/ports/announcement-public-reader.port';
-import { GetActiveAnnouncementsUseCase } from './application/use-cases/get-active-announcements.use-case';
-import { GetAnnouncementByIdUseCase } from './application/use-cases/get-announcement-by-id.use-case';
-import { AnnouncementItemMapperService } from './domain/services/announcement-item-mapper.service';
-import { AnnouncementPageAssemblerService } from './domain/services/announcement-page-assembler.service';
-import { AnnouncementPaginationAssemblerService } from './domain/services/announcement-pagination-assembler.service';
-import { AnnouncementMongoosePublicReaderAdapter } from './infrastructure/announcement-mongoose-public-reader.adapter';
-import { ANNOUNCEMENT_ADMIN_READER_PORT } from './admin/application/ports/announcement-admin-reader.port';
-import { ANNOUNCEMENT_WRITER_PORT } from './admin/application/ports/announcement-writer.port';
-import { GetAllAnnouncementsUseCase } from './admin/application/use-cases/get-all-announcements.use-case';
-import { CreateAnnouncementUseCase } from './admin/application/use-cases/create-announcement.use-case';
-import { UpdateAnnouncementUseCase } from './admin/application/use-cases/update-announcement.use-case';
-import { DeleteAnnouncementUseCase } from './admin/application/use-cases/delete-announcement.use-case';
-import { AnnouncementMongooseAdminReaderAdapter } from './admin/infrastructure/announcement-mongoose-admin-reader.adapter';
-import { AnnouncementMongooseWriterAdapter } from './admin/infrastructure/announcement-mongoose-writer.adapter';
-import { AnnouncementRepository } from './repository/announcement.repository';
-
-import { Announcement, AnnouncementSchema } from '../../schema/announcement.schema';
+import {
+    ANNOUNCEMENT_MODULE_CONTROLLERS,
+    ANNOUNCEMENT_MODULE_IMPORTS,
+    ANNOUNCEMENT_MODULE_PROVIDERS,
+} from './announcement.module-definition';
 
 /**
  * 공지사항 모듈
  * 공개 API 및 관리자 API 제공
  */
 @Module({
-    imports: [MongooseModule.forFeature([{ name: Announcement.name, schema: AnnouncementSchema }])],
-    controllers: [AnnouncementController, AnnouncementAdminQueryController, AnnouncementAdminCommandController],
-    providers: [
-        GetActiveAnnouncementsUseCase,
-        GetAnnouncementByIdUseCase,
-        GetAllAnnouncementsUseCase,
-        CreateAnnouncementUseCase,
-        UpdateAnnouncementUseCase,
-        DeleteAnnouncementUseCase,
-        AnnouncementItemMapperService,
-        AnnouncementPageAssemblerService,
-        AnnouncementPaginationAssemblerService,
-        AnnouncementRepository,
-        AnnouncementMongoosePublicReaderAdapter,
-        AnnouncementMongooseAdminReaderAdapter,
-        AnnouncementMongooseWriterAdapter,
-        {
-            provide: ANNOUNCEMENT_PUBLIC_READER_PORT,
-            useExisting: AnnouncementMongoosePublicReaderAdapter,
-        },
-        {
-            provide: ANNOUNCEMENT_ADMIN_READER_PORT,
-            useExisting: AnnouncementMongooseAdminReaderAdapter,
-        },
-        {
-            provide: ANNOUNCEMENT_WRITER_PORT,
-            useExisting: AnnouncementMongooseWriterAdapter,
-        },
-        CustomLoggerService,
-    ],
+    imports: ANNOUNCEMENT_MODULE_IMPORTS,
+    controllers: ANNOUNCEMENT_MODULE_CONTROLLERS,
+    providers: ANNOUNCEMENT_MODULE_PROVIDERS,
 })
 export class AnnouncementModule {}
