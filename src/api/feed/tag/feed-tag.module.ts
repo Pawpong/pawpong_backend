a5d2128617cb97ce754df092211d07198fe9fe74
KIM SEUNG CHAN <201921339@sangmyung.kr>
2026-04-14 16:16:55 +0900

    refactor(feed-tag): 모듈 정의를 역할별 배열로 분리
    - feed-tag module의 schema import와 export binding 구성을 definition 파일로 이동해 선언부를 얇게 정리함
    - use-case, domain, infrastructure, port binding provider를 역할 기준 배열로 나눠 태그 상호작용 구성을 읽기 쉽게 정리함

--- src/api/feed/tag/feed-tag.module.ts
+++ src/api/feed/tag/feed-tag.module.ts
@@ -1,24 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Video, VideoSchema } from '../../../schema/video.schema';
-import { StorageModule } from '../../../common/storage/storage.module';
-import { FeedCacheKeyService } from '../domain/services/feed-cache-key.service';
-import { FeedVideoSummaryMapperService } from '../domain/services/feed-video-summary-mapper.service';
-import { FEED_TAG_ASSET_URL_PORT } from './application/ports/feed-tag-asset-url.port';
+
 import {
-    GET_POPULAR_FEED_TAGS_USE_CASE,
-    SEARCH_FEED_VIDEOS_BY_TAG_USE_CASE,
-    SUGGEST_FEED_TAGS_USE_CASE,
-} from './application/tokens/feed-tag-interaction.token';
-import { SearchByTagUseCase } from './application/use-cases/search-by-tag.use-case';
-import { GetPopularTagsUseCase } from './application/use-cases/get-popular-tags.use-case';
-import { SuggestTagsUseCase } from './application/use-cases/suggest-tags.use-case';
-import { FeedTagNormalizerService } from './domain/services/feed-tag-normalizer.service';
-import { FeedTagSearchResultAssemblerService } from './domain/services/feed-tag-search-result-assembler.service';
-import { FeedTagMongooseReaderAdapter } from './infrastructure/feed-tag-mongoose-reader.adapter';
-import { FeedTagStorageAssetUrlAdapter } from './infrastructure/feed-tag-storage-asset-url.adapter';
-import { FeedTagRepository } from './repository/feed-tag.repository';
-import { FEED_TAG_READER_PORT } from './application/ports/feed-tag-reader.port';
+    FEED_TAG_MODULE_EXPORTS,
+    FEED_TAG_MODULE_IMPORTS,
+    FEED_TAG_MODULE_PROVIDERS,
+} from './feed-tag.module-definition';
 
 /**
  * 피드 태그 모듈
@@ -27,39 +13,8 @@ import { FEED_TAG_READER_PORT } from './application/ports/feed-tag-reader.port';
  * - 태그 자동완성
  */
 @Module({
-    imports: [MongooseModule.forFeature([{ name: Video.name, schema: VideoSchema }]), StorageModule],
-    providers: [
-        SearchByTagUseCase,
-        GetPopularTagsUseCase,
-        SuggestTagsUseCase,
-        FeedCacheKeyService,
-        FeedVideoSummaryMapperService,
-        FeedTagNormalizerService,
-        FeedTagSearchResultAssemblerService,
-        FeedTagRepository,
-        FeedTagMongooseReaderAdapter,
-        FeedTagStorageAssetUrlAdapter,
-        {
-            provide: FEED_TAG_READER_PORT,
-            useExisting: FeedTagMongooseReaderAdapter,
-        },
-        {
-            provide: FEED_TAG_ASSET_URL_PORT,
-            useExisting: FeedTagStorageAssetUrlAdapter,
-        },
-        {
-            provide: SEARCH_FEED_VIDEOS_BY_TAG_USE_CASE,
-            useExisting: SearchByTagUseCase,
-        },
-        {
-            provide: GET_POPULAR_FEED_TAGS_USE_CASE,
-            useExisting: GetPopularTagsUseCase,
-        },
-        {
-            provide: SUGGEST_FEED_TAGS_USE_CASE,
-            useExisting: SuggestTagsUseCase,
-        },
-    ],
-    exports: [SEARCH_FEED_VIDEOS_BY_TAG_USE_CASE, GET_POPULAR_FEED_TAGS_USE_CASE, SUGGEST_FEED_TAGS_USE_CASE],
+    imports: FEED_TAG_MODULE_IMPORTS,
+    providers: FEED_TAG_MODULE_PROVIDERS,
+    exports: FEED_TAG_MODULE_EXPORTS,
 })
 export class FeedTagModule {}
