e38aea13d17fc0f6fb81f40a65acb00529bc572d
KIM SEUNG CHAN <201921339@sangmyung.kr>
2026-04-14 14:09:30 +0900

    refactor(user-admin): 모듈 정의를 그룹별 배열로 분리
    - user-admin module의 schema import와 controller 구성을 definition 파일로 이동해 선언부를 얇게 정리함
    - use-case, domain, infrastructure, port binding provider를 역할 기준 배열로 나눠 관리자 기능 구성을 읽기 쉽게 정리함

--- src/api/user/admin/user-admin.module.ts
+++ src/api/user/admin/user-admin.module.ts
@@ -1,46 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 
-import { UserAdminDeletedUsersListController } from './user-admin-deleted-users-list.controller';
-import { UserAdminDeletedUserStatsController } from './user-admin-deleted-user-stats.controller';
-import { UserAdminHardDeleteUserController } from './user-admin-hard-delete-user.controller';
-import { UserAdminPhoneWhitelistCreateController } from './user-admin-phone-whitelist-create.controller';
-import { UserAdminPhoneWhitelistDeleteController } from './user-admin-phone-whitelist-delete.controller';
-import { UserAdminPhoneWhitelistListController } from './user-admin-phone-whitelist-list.controller';
-import { UserAdminPhoneWhitelistUpdateController } from './user-admin-phone-whitelist-update.controller';
-import { UserAdminProfileController } from './user-admin-profile.controller';
-import { UserAdminRestoreUserController } from './user-admin-restore-user.controller';
-import { UserAdminUsersListController } from './user-admin-users-list.controller';
-import { UserAdminUserStatusController } from './user-admin-user-status.controller';
-import { GetAdminProfileUseCase } from './application/use-cases/get-admin-profile.use-case';
-import { GetUsersUseCase } from './application/use-cases/get-users.use-case';
-import { UpdateUserStatusUseCase } from './application/use-cases/update-user-status.use-case';
-import { GetDeletedUsersUseCase } from './application/use-cases/get-deleted-users.use-case';
-import { GetDeletedUserStatsUseCase } from './application/use-cases/get-deleted-user-stats.use-case';
-import { RestoreDeletedUserUseCase } from './application/use-cases/restore-deleted-user.use-case';
-import { HardDeleteUserUseCase } from './application/use-cases/hard-delete-user.use-case';
-import { GetPhoneWhitelistUseCase } from './application/use-cases/get-phone-whitelist.use-case';
-import { AddPhoneWhitelistUseCase } from './application/use-cases/add-phone-whitelist.use-case';
-import { UpdatePhoneWhitelistUseCase } from './application/use-cases/update-phone-whitelist.use-case';
-import { DeletePhoneWhitelistUseCase } from './application/use-cases/delete-phone-whitelist.use-case';
-import { USER_ADMIN_READER_PORT } from './application/ports/user-admin-reader.port';
-import { USER_ADMIN_WRITER_PORT } from './application/ports/user-admin-writer.port';
-import { UserAdminCommandPolicyService } from './domain/services/user-admin-command-policy.service';
-import { UserAdminDeletedUserCommandResultMapperService } from './domain/services/user-admin-deleted-user-command-result-mapper.service';
-import { UserAdminDeletedUserPageAssemblerService } from './domain/services/user-admin-deleted-user-page-assembler.service';
-import { UserAdminDeletedUserStatsResultMapperService } from './domain/services/user-admin-deleted-user-stats-result-mapper.service';
-import { UserAdminPaginationAssemblerService } from './domain/services/user-admin-pagination-assembler.service';
-import { UserAdminPhoneWhitelistResultMapperService } from './domain/services/user-admin-phone-whitelist-result-mapper.service';
-import { UserAdminAdminProfileMapperService } from './domain/services/user-admin-admin-profile-mapper.service';
-import { UserAdminUserPageAssemblerService } from './domain/services/user-admin-user-page-assembler.service';
-import { UserAdminActivityLogFactoryService } from './domain/services/user-admin-activity-log-factory.service';
-import { UserAdminMongooseRepositoryAdapter } from './infrastructure/user-admin-mongoose.repository.adapter';
-import { UserAdminRepository } from './repository/user-admin.repository';
-
-import { Admin, AdminSchema } from '../../../schema/admin.schema';
-import { Breeder, BreederSchema } from '../../../schema/breeder.schema';
-import { Adopter, AdopterSchema } from '../../../schema/adopter.schema';
-import { PhoneWhitelist, PhoneWhitelistSchema } from '../../../schema/phone-whitelist.schema';
+import {
+    USER_ADMIN_MODULE_CONTROLLERS,
+    USER_ADMIN_MODULE_IMPORTS,
+    USER_ADMIN_MODULE_PROVIDERS,
+} from './user-admin.module-definition';
 
 /**
  * 사용자 관리 Admin 모듈
@@ -53,58 +17,8 @@ import { PhoneWhitelist, PhoneWhitelistSchema } from '../../../schema/phone-whit
  * - 전화번호 화이트리스트 관리
  */
 @Module({
-    imports: [
-        MongooseModule.forFeature([
-            { name: Admin.name, schema: AdminSchema },
-            { name: Breeder.name, schema: BreederSchema },
-            { name: Adopter.name, schema: AdopterSchema },
-            { name: PhoneWhitelist.name, schema: PhoneWhitelistSchema },
-        ]),
-    ],
-    controllers: [
-        UserAdminProfileController,
-        UserAdminUsersListController,
-        UserAdminUserStatusController,
-        UserAdminDeletedUsersListController,
-        UserAdminDeletedUserStatsController,
-        UserAdminRestoreUserController,
-        UserAdminHardDeleteUserController,
-        UserAdminPhoneWhitelistListController,
-        UserAdminPhoneWhitelistCreateController,
-        UserAdminPhoneWhitelistUpdateController,
-        UserAdminPhoneWhitelistDeleteController,
-    ],
-    providers: [
-        GetAdminProfileUseCase,
-        GetUsersUseCase,
-        UpdateUserStatusUseCase,
-        GetDeletedUsersUseCase,
-        GetDeletedUserStatsUseCase,
-        RestoreDeletedUserUseCase,
-        HardDeleteUserUseCase,
-        GetPhoneWhitelistUseCase,
-        AddPhoneWhitelistUseCase,
-        UpdatePhoneWhitelistUseCase,
-        DeletePhoneWhitelistUseCase,
-        UserAdminCommandPolicyService,
-        UserAdminDeletedUserPageAssemblerService,
-        UserAdminDeletedUserStatsResultMapperService,
-        UserAdminDeletedUserCommandResultMapperService,
-        UserAdminPaginationAssemblerService,
-        UserAdminPhoneWhitelistResultMapperService,
-        UserAdminAdminProfileMapperService,
-        UserAdminUserPageAssemblerService,
-        UserAdminActivityLogFactoryService,
-        UserAdminRepository,
-        UserAdminMongooseRepositoryAdapter,
-        {
-            provide: USER_ADMIN_READER_PORT,
-            useExisting: UserAdminMongooseRepositoryAdapter,
-        },
-        {
-            provide: USER_ADMIN_WRITER_PORT,
-            useExisting: UserAdminMongooseRepositoryAdapter,
-        },
-    ],
+    imports: USER_ADMIN_MODULE_IMPORTS,
+    controllers: USER_ADMIN_MODULE_CONTROLLERS,
+    providers: USER_ADMIN_MODULE_PROVIDERS,
 })
 export class UserAdminModule {}
