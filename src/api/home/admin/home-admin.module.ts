e0927dd08dd668b0397f5791d6bcd9b360496124
KIM SEUNG CHAN <201921339@sangmyung.kr>
2026-04-14 16:11:06 +0900

    refactor(home-admin): 모듈 정의를 역할별 배열로 분리
    - home-admin module의 schema import와 배너/FAQ controller 구성을 definition 파일로 이동해 선언부를 얇게 정리함
    - use-case, domain, infrastructure, port binding provider를 역할 기준 배열로 나눠 관리자 홈 구성을 읽기 쉽게 정리함

--- src/api/home/admin/home-admin.module.ts
+++ src/api/home/admin/home-admin.module.ts
@@ -1,69 +1,14 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 
-import { HomeAdminBannersCommandController } from './home-admin-banners-command.controller';
-import { HomeAdminBannersQueryController } from './home-admin-banners-query.controller';
-import { HomeAdminFaqsCommandController } from './home-admin-faqs-command.controller';
-import { HomeAdminFaqsQueryController } from './home-admin-faqs-query.controller';
-import { GetAllBannersUseCase } from './application/use-cases/get-all-banners.use-case';
-import { CreateBannerUseCase } from './application/use-cases/create-banner.use-case';
-import { UpdateBannerUseCase } from './application/use-cases/update-banner.use-case';
-import { DeleteBannerUseCase } from './application/use-cases/delete-banner.use-case';
-import { GetAllFaqsUseCase } from './application/use-cases/get-all-faqs.use-case';
-import { CreateFaqUseCase } from './application/use-cases/create-faq.use-case';
-import { UpdateFaqUseCase } from './application/use-cases/update-faq.use-case';
-import { DeleteFaqUseCase } from './application/use-cases/delete-faq.use-case';
-import { HOME_ASSET_URL_PORT } from '../application/ports/home-asset-url.port';
-import { HomeAdminMongooseManagerAdapter } from './infrastructure/home-admin-mongoose-manager.adapter';
-import { HOME_ADMIN_MANAGER_PORT } from './application/ports/home-admin-manager.port';
-import { HomeBannerCatalogService } from '../domain/services/home-banner-catalog.service';
-import { HomeFaqCatalogService } from '../domain/services/home-faq-catalog.service';
-import { HomeStorageAssetUrlAdapter } from '../infrastructure/home-storage-asset-url.adapter';
-import { BannerRepository } from '../repository/banner.repository';
-import { FaqRepository } from '../repository/faq.repository';
-
-import { Faq, FaqSchema } from '../../../schema/faq.schema';
-import { Banner, BannerSchema } from '../../../schema/banner.schema';
-
-import { StorageModule } from '../../../common/storage/storage.module';
+import {
+    HOME_ADMIN_MODULE_CONTROLLERS,
+    HOME_ADMIN_MODULE_IMPORTS,
+    HOME_ADMIN_MODULE_PROVIDERS,
+} from './home-admin.module-definition';
 
 @Module({
-    imports: [
-        MongooseModule.forFeature([
-            { name: Banner.name, schema: BannerSchema },
-            { name: Faq.name, schema: FaqSchema },
-        ]),
-        StorageModule,
-    ],
-    controllers: [
-        HomeAdminBannersQueryController,
-        HomeAdminBannersCommandController,
-        HomeAdminFaqsQueryController,
-        HomeAdminFaqsCommandController,
-    ],
-    providers: [
-        GetAllBannersUseCase,
-        CreateBannerUseCase,
-        UpdateBannerUseCase,
-        DeleteBannerUseCase,
-        GetAllFaqsUseCase,
-        CreateFaqUseCase,
-        UpdateFaqUseCase,
-        DeleteFaqUseCase,
-        HomeBannerCatalogService,
-        HomeFaqCatalogService,
-        BannerRepository,
-        FaqRepository,
-        HomeAdminMongooseManagerAdapter,
-        HomeStorageAssetUrlAdapter,
-        {
-            provide: HOME_ADMIN_MANAGER_PORT,
-            useExisting: HomeAdminMongooseManagerAdapter,
-        },
-        {
-            provide: HOME_ASSET_URL_PORT,
-            useExisting: HomeStorageAssetUrlAdapter,
-        },
-    ],
+    imports: HOME_ADMIN_MODULE_IMPORTS,
+    controllers: HOME_ADMIN_MODULE_CONTROLLERS,
+    providers: HOME_ADMIN_MODULE_PROVIDERS,
 })
 export class HomeAdminModule {}
