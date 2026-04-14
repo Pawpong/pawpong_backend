de2aaa36b10404966d1ee466ab38c62f26fc4482
KIM SEUNG CHAN <201921339@sangmyung.kr>
2026-04-14 15:18:04 +0900

    refactor(feed-comment): 모듈 정의를 역할별 배열로 분리
    - feed-comment module의 schema import와 export binding 구성을 definition 파일로 이동해 선언부를 얇게 정리함
    - use-case, domain, infrastructure, port binding provider를 역할 기준 배열로 나눠 댓글 상호작용 구성을 읽기 쉽게 정리함

--- src/api/feed/comment/feed-comment.module.ts
+++ src/api/feed/comment/feed-comment.module.ts
@@ -1,26 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Video, VideoSchema } from '../../../schema/video.schema';
-import { VideoComment, VideoCommentSchema } from '../../../schema/video-comment.schema';
-import { FeedCacheKeyService } from '../domain/services/feed-cache-key.service';
-import { CreateCommentUseCase } from './application/use-cases/create-comment.use-case';
-import { GetCommentsUseCase } from './application/use-cases/get-comments.use-case';
-import { GetRepliesUseCase } from './application/use-cases/get-replies.use-case';
-import { UpdateCommentUseCase } from './application/use-cases/update-comment.use-case';
-import { DeleteCommentUseCase } from './application/use-cases/delete-comment.use-case';
+
 import {
-    CREATE_FEED_VIDEO_COMMENT_USE_CASE,
-    DELETE_FEED_VIDEO_COMMENT_USE_CASE,
-    GET_FEED_VIDEO_COMMENTS_USE_CASE,
-    GET_FEED_VIDEO_REPLIES_USE_CASE,
-    UPDATE_FEED_VIDEO_COMMENT_USE_CASE,
-} from './application/tokens/feed-comment-interaction.token';
-import { FeedCommentCommandResultMapperService } from './domain/services/feed-comment-command-result-mapper.service';
-import { FeedCommentPageAssemblerService } from './domain/services/feed-comment-page-assembler.service';
-import { FeedCommentPolicyService } from './domain/services/feed-comment-policy.service';
-import { FeedCommentMongooseManagerAdapter } from './infrastructure/feed-comment-mongoose-manager.adapter';
-import { FeedCommentRepository } from './repository/feed-comment.repository';
-import { FEED_COMMENT_MANAGER_PORT } from './application/ports/feed-comment-manager.port';
+    FEED_COMMENT_MODULE_EXPORTS,
+    FEED_COMMENT_MODULE_IMPORTS,
+    FEED_COMMENT_MODULE_PROVIDERS,
+} from './feed-comment.module-definition';
 
 /**
  * 피드 댓글 모듈
@@ -28,55 +12,8 @@ import { FEED_COMMENT_MANAGER_PORT } from './application/ports/feed-comment-mana
  * - 대댓글 기능
  */
 @Module({
-    imports: [
-        MongooseModule.forFeature([
-            { name: Video.name, schema: VideoSchema },
-            { name: VideoComment.name, schema: VideoCommentSchema },
-        ]),
-    ],
-    providers: [
-        CreateCommentUseCase,
-        GetCommentsUseCase,
-        GetRepliesUseCase,
-        UpdateCommentUseCase,
-        DeleteCommentUseCase,
-        FeedCacheKeyService,
-        FeedCommentPolicyService,
-        FeedCommentCommandResultMapperService,
-        FeedCommentPageAssemblerService,
-        FeedCommentRepository,
-        FeedCommentMongooseManagerAdapter,
-        {
-            provide: FEED_COMMENT_MANAGER_PORT,
-            useExisting: FeedCommentMongooseManagerAdapter,
-        },
-        {
-            provide: CREATE_FEED_VIDEO_COMMENT_USE_CASE,
-            useExisting: CreateCommentUseCase,
-        },
-        {
-            provide: GET_FEED_VIDEO_COMMENTS_USE_CASE,
-            useExisting: GetCommentsUseCase,
-        },
-        {
-            provide: GET_FEED_VIDEO_REPLIES_USE_CASE,
-            useExisting: GetRepliesUseCase,
-        },
-        {
-            provide: UPDATE_FEED_VIDEO_COMMENT_USE_CASE,
-            useExisting: UpdateCommentUseCase,
-        },
-        {
-            provide: DELETE_FEED_VIDEO_COMMENT_USE_CASE,
-            useExisting: DeleteCommentUseCase,
-        },
-    ],
-    exports: [
-        CREATE_FEED_VIDEO_COMMENT_USE_CASE,
-        GET_FEED_VIDEO_COMMENTS_USE_CASE,
-        GET_FEED_VIDEO_REPLIES_USE_CASE,
-        UPDATE_FEED_VIDEO_COMMENT_USE_CASE,
-        DELETE_FEED_VIDEO_COMMENT_USE_CASE,
-    ],
+    imports: FEED_COMMENT_MODULE_IMPORTS,
+    providers: FEED_COMMENT_MODULE_PROVIDERS,
+    exports: FEED_COMMENT_MODULE_EXPORTS,
 })
 export class FeedCommentModule {}
