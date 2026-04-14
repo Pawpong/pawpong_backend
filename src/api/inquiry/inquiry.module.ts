4749cd16f1a5a0d3169d1b004b037afc84971a30
KIM SEUNG CHAN <201921339@sangmyung.kr>
2026-04-14 14:22:01 +0900

    refactor(inquiry): 모듈 정의를 역할별 배열로 분리
    - inquiry module의 schema import와 controller 구성을 definition 파일로 이동해 선언부를 얇게 정리함
    - use-case, domain, infrastructure, port binding provider를 역할 기준 배열로 나눠 문의 기능 구성을 읽기 쉽게 정리함

--- src/api/inquiry/inquiry.module.ts
+++ src/api/inquiry/inquiry.module.ts
@@ -1,85 +1,18 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 
-import { InquiryAdopterCommandController } from './inquiry-adopter-command.controller';
-import { InquiryAdopterQueryController } from './inquiry-adopter-query.controller';
-import { InquiryBreederAnswerController } from './inquiry-breeder-answer.controller';
-import { InquiryBreederQueryController } from './inquiry-breeder-query.controller';
-import { InquiryPublicDetailController } from './inquiry-public-detail.controller';
-import { InquiryPublicListController } from './inquiry-public-list.controller';
-
-import { InquiryRepository } from './repository/inquiry.repository';
-import { CreateInquiryUseCase } from './application/use-cases/create-inquiry.use-case';
-import { UpdateInquiryUseCase } from './application/use-cases/update-inquiry.use-case';
-import { DeleteInquiryUseCase } from './application/use-cases/delete-inquiry.use-case';
-import { CreateInquiryAnswerUseCase } from './application/use-cases/create-inquiry-answer.use-case';
-import { GetBreederInquiriesUseCase } from './application/use-cases/get-breeder-inquiries.use-case';
-import { GetInquiryDetailUseCase } from './application/use-cases/get-inquiry-detail.use-case';
-import { GetInquiryListUseCase } from './application/use-cases/get-inquiry-list.use-case';
-import { GetMyInquiriesUseCase } from './application/use-cases/get-my-inquiries.use-case';
-import { INQUIRY_ASSET_URL_PORT } from './application/ports/inquiry-asset-url.port';
-import { InquiryCommandPolicyService } from './domain/services/inquiry-command-policy.service';
-import { InquiryViewService } from './domain/services/inquiry-view.service';
-import { INQUIRY_COMMAND_PORT } from './application/ports/inquiry-command.port';
-import { InquiryRepositoryCommandAdapter } from './infrastructure/inquiry-repository-command.adapter';
-import { InquiryRepositoryReaderAdapter } from './infrastructure/inquiry-repository-reader.adapter';
-import { InquiryStorageAssetUrlAdapter } from './infrastructure/inquiry-storage-asset-url.adapter';
-import { INQUIRY_READER_PORT } from './application/ports/inquiry-reader.port';
-
-import { Inquiry, InquirySchema } from '../../schema/inquiry.schema';
-import { Adopter, AdopterSchema } from '../../schema/adopter.schema';
-import { Breeder, BreederSchema } from '../../schema/breeder.schema';
-
-import { StorageModule } from '../../common/storage/storage.module';
+import {
+    INQUIRY_MODULE_CONTROLLERS,
+    INQUIRY_MODULE_IMPORTS,
+    INQUIRY_MODULE_PROVIDERS,
+} from './inquiry.module-definition';
 
 /**
  * 문의 모듈
  * 입양자 질문 작성, 브리더 답변, 목록/상세 조회 기능 제공
  */
 @Module({
-    imports: [
-        MongooseModule.forFeature([
-            { name: Inquiry.name, schema: InquirySchema },
-            { name: Adopter.name, schema: AdopterSchema },
-            { name: Breeder.name, schema: BreederSchema },
-        ]),
-        StorageModule,
-    ],
-    controllers: [
-        InquiryAdopterQueryController,
-        InquiryAdopterCommandController,
-        InquiryBreederQueryController,
-        InquiryBreederAnswerController,
-        InquiryPublicListController,
-        InquiryPublicDetailController,
-    ],
-    providers: [
-        InquiryRepository,
-        GetInquiryListUseCase,
-        GetInquiryDetailUseCase,
-        GetMyInquiriesUseCase,
-        GetBreederInquiriesUseCase,
-        CreateInquiryUseCase,
-        UpdateInquiryUseCase,
-        DeleteInquiryUseCase,
-        CreateInquiryAnswerUseCase,
-        InquiryCommandPolicyService,
-        InquiryViewService,
-        InquiryRepositoryReaderAdapter,
-        InquiryRepositoryCommandAdapter,
-        InquiryStorageAssetUrlAdapter,
-        {
-            provide: INQUIRY_READER_PORT,
-            useExisting: InquiryRepositoryReaderAdapter,
-        },
-        {
-            provide: INQUIRY_COMMAND_PORT,
-            useExisting: InquiryRepositoryCommandAdapter,
-        },
-        {
-            provide: INQUIRY_ASSET_URL_PORT,
-            useExisting: InquiryStorageAssetUrlAdapter,
-        },
-    ],
+    imports: INQUIRY_MODULE_IMPORTS,
+    controllers: INQUIRY_MODULE_CONTROLLERS,
+    providers: INQUIRY_MODULE_PROVIDERS,
 })
 export class InquiryModule {}
