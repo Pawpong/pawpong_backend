9b8f6703284a105f7447bde871b647bd75635443
KIM SEUNG CHAN <201921339@sangmyung.kr>
2026-04-14 14:06:13 +0900

    refactor(notification): 모듈 정의와 이메일 미리보기 컨트롤러 위치를 정리
    - production 모듈이 test 폴더 컨트롤러를 직접 import하던 구조를 제거하고 이메일 미리보기 컨트롤러를 proper 위치로 이동함
    - notification module 정의를 import/controller/provider/export 배열로 분리해 의존 구성을 읽기 쉽게 정리함

--- src/api/notification/notification.module.ts
+++ src/api/notification/notification.module.ts
@@ -1,43 +1,11 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 
-import { EmailTestController } from './test/email-test.controller';
-import { NotificationDeleteController } from './notification-delete.controller';
-import { NotificationListController } from './notification-list.controller';
-import { NotificationMarkAllReadController } from './notification-mark-all-read.controller';
-import { NotificationMarkReadController } from './notification-mark-read.controller';
-import { NotificationUnreadCountController } from './notification-unread-count.controller';
-import { NOTIFICATION_DISPATCH_PORT } from './application/ports/notification-dispatch.port';
-import { NotificationDispatchService } from './application/services/notification-dispatch.service';
-import { CreateNotificationUseCase } from './application/use-cases/create-notification.use-case';
-import { CreateNotificationFromBuilderUseCase } from './application/use-cases/create-notification-from-builder.use-case';
-import { GetNotificationsUseCase } from './application/use-cases/get-notifications.use-case';
-import { GetUnreadNotificationCountUseCase } from './application/use-cases/get-unread-notification-count.use-case';
-import { MarkNotificationReadUseCase } from './application/use-cases/mark-notification-read.use-case';
-import { MarkAllNotificationsReadUseCase } from './application/use-cases/mark-all-notifications-read.use-case';
-import { DeleteNotificationUseCase } from './application/use-cases/delete-notification.use-case';
-import { SendNotificationEmailUseCase } from './application/use-cases/send-notification-email.use-case';
-import { NotificationItemMapperService } from './domain/services/notification-item-mapper.service';
-import { NotificationPageAssemblerService } from './domain/services/notification-page-assembler.service';
-import { NotificationPaginationAssemblerService } from './domain/services/notification-pagination-assembler.service';
-import { NotificationMessageTemplateService } from './domain/services/notification-message-template.service';
-import { NotificationStateResultMapperService } from './domain/services/notification-state-result-mapper.service';
-import { NotificationMongooseInboxAdapter } from './infrastructure/notification-mongoose-inbox.adapter';
-import { NotificationMongooseCommandAdapter } from './infrastructure/notification-mongoose-command.adapter';
-import { NotificationMailAdapter } from './infrastructure/notification-mail.adapter';
-import { NotificationRepository } from './repository/notification.repository';
-import { NOTIFICATION_INBOX_PORT } from './application/ports/notification-inbox.port';
-import { NOTIFICATION_COMMAND_PORT } from './application/ports/notification-command.port';
-import { NOTIFICATION_EMAIL_PORT } from './application/ports/notification-email.port';
 import {
-    CREATE_NOTIFICATION_DISPATCH_USE_CASE,
-    CREATE_NOTIFICATION_FROM_BUILDER_DISPATCH_USE_CASE,
-    SEND_NOTIFICATION_EMAIL_DISPATCH_USE_CASE,
-} from './application/tokens/notification-dispatch-use-case.token';
-
-import { Notification, NotificationSchema } from '../../schema/notification.schema';
-
-import { MailModule } from '../../common/mail/mail.module';
+    NOTIFICATION_MODULE_CONTROLLERS,
+    NOTIFICATION_MODULE_EXPORTS,
+    NOTIFICATION_MODULE_IMPORTS,
+    NOTIFICATION_MODULE_PROVIDERS,
+} from './notification.module-definition';
 
 /**
  * 알림 모듈
@@ -45,63 +13,9 @@ import { MailModule } from '../../common/mail/mail.module';
  * 서비스 알림 및 이메일 발송 기능을 통합 제공합니다.
  */
 @Module({
-    imports: [MongooseModule.forFeature([{ name: Notification.name, schema: NotificationSchema }]), MailModule],
-    controllers: [
-        NotificationListController,
-        NotificationUnreadCountController,
-        NotificationMarkReadController,
-        NotificationMarkAllReadController,
-        NotificationDeleteController,
-        EmailTestController,
-    ],
-    providers: [
-        NotificationRepository,
-        NotificationDispatchService,
-        CreateNotificationUseCase,
-        CreateNotificationFromBuilderUseCase,
-        NotificationItemMapperService,
-        NotificationPageAssemblerService,
-        NotificationPaginationAssemblerService,
-        NotificationMessageTemplateService,
-        NotificationStateResultMapperService,
-        NotificationMongooseInboxAdapter,
-        NotificationMongooseCommandAdapter,
-        NotificationMailAdapter,
-        GetNotificationsUseCase,
-        GetUnreadNotificationCountUseCase,
-        MarkNotificationReadUseCase,
-        MarkAllNotificationsReadUseCase,
-        DeleteNotificationUseCase,
-        SendNotificationEmailUseCase,
-        {
-            provide: NOTIFICATION_INBOX_PORT,
-            useExisting: NotificationMongooseInboxAdapter,
-        },
-        {
-            provide: NOTIFICATION_COMMAND_PORT,
-            useExisting: NotificationMongooseCommandAdapter,
-        },
-        {
-            provide: NOTIFICATION_EMAIL_PORT,
-            useExisting: NotificationMailAdapter,
-        },
-        {
-            provide: CREATE_NOTIFICATION_DISPATCH_USE_CASE,
-            useExisting: CreateNotificationUseCase,
-        },
-        {
-            provide: CREATE_NOTIFICATION_FROM_BUILDER_DISPATCH_USE_CASE,
-            useExisting: CreateNotificationFromBuilderUseCase,
-        },
-        {
-            provide: SEND_NOTIFICATION_EMAIL_DISPATCH_USE_CASE,
-            useExisting: SendNotificationEmailUseCase,
-        },
-        {
-            provide: NOTIFICATION_DISPATCH_PORT,
-            useExisting: NotificationDispatchService,
-        },
-    ],
-    exports: [NOTIFICATION_DISPATCH_PORT],
+    imports: NOTIFICATION_MODULE_IMPORTS,
+    controllers: NOTIFICATION_MODULE_CONTROLLERS,
+    providers: NOTIFICATION_MODULE_PROVIDERS,
+    exports: NOTIFICATION_MODULE_EXPORTS,
 })
 export class NotificationModule {}
