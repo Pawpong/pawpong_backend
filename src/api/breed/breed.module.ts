344403216bbff972aa1fb748cb720a4ec91bc307
KIM SEUNG CHAN <201921339@sangmyung.kr>
2026-04-14 16:17:11 +0900

    refactor(breed): 모듈 정의를 역할별 배열로 분리
    - breed module의 public/admin controller 구성을 definition 파일로 이동해 선언부를 얇게 정리함
    - use-case, domain, infrastructure, port binding provider를 역할 기준 배열로 나눠 품종 조회와 관리 구성을 읽기 쉽게 정리함

--- src/api/breed/breed.module.ts
+++ src/api/breed/breed.module.ts
@@ -1,56 +1,14 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 
-import { BreedController } from './breed.controller';
-import { BreedAdminCommandController } from './admin/breed-admin-command.controller';
-import { BreedAdminQueryController } from './admin/breed-admin-query.controller';
-
-import { GetBreedsUseCase } from './application/use-cases/get-breeds.use-case';
-import { BreedCatalogService } from './domain/services/breed-catalog.service';
-import { BreedAdminResultMapperService } from './domain/services/breed-admin-result-mapper.service';
-import { BreedMongooseReaderAdapter } from './infrastructure/breed-mongoose-reader.adapter';
-import { BREED_READER_PORT } from './application/ports/breed-reader.port';
-import { BREED_ADMIN_READER_PORT } from './admin/application/ports/breed-admin-reader.port';
-import { BREED_WRITER_PORT } from './admin/application/ports/breed-writer.port';
-import { CreateBreedUseCase } from './admin/application/use-cases/create-breed.use-case';
-import { GetAllBreedsAdminUseCase } from './admin/application/use-cases/get-all-breeds-admin.use-case';
-import { GetBreedByIdUseCase } from './admin/application/use-cases/get-breed-by-id.use-case';
-import { UpdateBreedUseCase } from './admin/application/use-cases/update-breed.use-case';
-import { DeleteBreedUseCase } from './admin/application/use-cases/delete-breed.use-case';
-import { BreedMongooseAdminReaderAdapter } from './admin/infrastructure/breed-mongoose-admin-reader.adapter';
-import { BreedMongooseWriterAdapter } from './admin/infrastructure/breed-mongoose-writer.adapter';
-import { BreedRepository } from './repository/breed.repository';
-
-import { Breed, BreedSchema } from '../../schema/breed.schema';
+import {
+    BREED_MODULE_CONTROLLERS,
+    BREED_MODULE_IMPORTS,
+    BREED_MODULE_PROVIDERS,
+} from './breed.module-definition';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: Breed.name, schema: BreedSchema }])],
-    controllers: [BreedController, BreedAdminQueryController, BreedAdminCommandController],
-    providers: [
-        GetBreedsUseCase,
-        CreateBreedUseCase,
-        GetAllBreedsAdminUseCase,
-        GetBreedByIdUseCase,
-        UpdateBreedUseCase,
-        DeleteBreedUseCase,
-        BreedCatalogService,
-        BreedAdminResultMapperService,
-        BreedRepository,
-        BreedMongooseReaderAdapter,
-        BreedMongooseAdminReaderAdapter,
-        BreedMongooseWriterAdapter,
-        {
-            provide: BREED_READER_PORT,
-            useExisting: BreedMongooseReaderAdapter,
-        },
-        {
-            provide: BREED_ADMIN_READER_PORT,
-            useExisting: BreedMongooseAdminReaderAdapter,
-        },
-        {
-            provide: BREED_WRITER_PORT,
-            useExisting: BreedMongooseWriterAdapter,
-        },
-    ],
+    imports: BREED_MODULE_IMPORTS,
+    controllers: BREED_MODULE_CONTROLLERS,
+    providers: BREED_MODULE_PROVIDERS,
 })
 export class BreedModule {}
