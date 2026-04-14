a3b1e4e6b2222b4958963c039e3642d6250afff8
KIM SEUNG CHAN <201921339@sangmyung.kr>
2026-04-14 16:17:19 +0900

    refactor(notice): 모듈 정의를 역할별 배열로 분리
    - notice module의 public/admin controller 구성을 definition 파일로 이동해 선언부를 얇게 정리함
    - use-case, domain, infrastructure, port binding provider를 역할 기준 배열로 나눠 공지 조회와 관리 구성을 읽기 쉽게 정리함

--- src/api/notice/notice.module.ts
+++ src/api/notice/notice.module.ts
@@ -1,52 +1,18 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Notice, NoticeSchema } from '../../schema/notice.schema';
-import { NoticeController } from './notice.controller';
-import { NoticeAdminCommandController } from './admin/notice-admin-command.controller';
-import { NoticeAdminQueryController } from './admin/notice-admin-query.controller';
-import { CustomLoggerService } from '../../common/logger/custom-logger.service';
-import { GetNoticeListUseCase } from './application/use-cases/get-notice-list.use-case';
-import { GetNoticeDetailUseCase } from './application/use-cases/get-notice-detail.use-case';
-import { NoticePaginationAssemblerService } from './domain/services/notice-pagination-assembler.service';
-import { NoticeItemMapperService } from './domain/services/notice-item-mapper.service';
-import { NoticePageAssemblerService } from './domain/services/notice-page-assembler.service';
-import { NoticeMongooseReaderAdapter } from './infrastructure/notice-mongoose-reader.adapter';
-import { NOTICE_READER_PORT } from './application/ports/notice-reader.port';
-import { CreateNoticeUseCase } from './admin/application/use-cases/create-notice.use-case';
-import { UpdateNoticeUseCase } from './admin/application/use-cases/update-notice.use-case';
-import { DeleteNoticeUseCase } from './admin/application/use-cases/delete-notice.use-case';
-import { NoticeMongooseWriterAdapter } from './admin/infrastructure/notice-mongoose-writer.adapter';
-import { NOTICE_WRITER_PORT } from './admin/application/ports/notice-writer.port';
-import { NoticeRepository } from './repository/notice.repository';
+
+import {
+    NOTICE_MODULE_CONTROLLERS,
+    NOTICE_MODULE_IMPORTS,
+    NOTICE_MODULE_PROVIDERS,
+} from './notice.module-definition';
 
 /**
  * 공지사항 모듈
  * 공지사항 관련 기능을 제공
  */
 @Module({
-    imports: [MongooseModule.forFeature([{ name: Notice.name, schema: NoticeSchema }])],
-    controllers: [NoticeController, NoticeAdminQueryController, NoticeAdminCommandController],
-    providers: [
-        CustomLoggerService,
-        GetNoticeListUseCase,
-        GetNoticeDetailUseCase,
-        NoticePaginationAssemblerService,
-        NoticeItemMapperService,
-        NoticePageAssemblerService,
-        NoticeRepository,
-        NoticeMongooseReaderAdapter,
-        CreateNoticeUseCase,
-        UpdateNoticeUseCase,
-        DeleteNoticeUseCase,
-        NoticeMongooseWriterAdapter,
-        {
-            provide: NOTICE_READER_PORT,
-            useExisting: NoticeMongooseReaderAdapter,
-        },
-        {
-            provide: NOTICE_WRITER_PORT,
-            useExisting: NoticeMongooseWriterAdapter,
-        },
-    ],
+    imports: NOTICE_MODULE_IMPORTS,
+    controllers: NOTICE_MODULE_CONTROLLERS,
+    providers: NOTICE_MODULE_PROVIDERS,
 })
 export class NoticeModule {}
