10132181e46cb1215b7bb08d3508bc8026214eec
heeyoung123 <heeyoung321@naver.com>
2026-09-21 22:36:18 +0900

    fix(community): 댓글 모듈에 NotificationModule import 누락 수정
    좋아요는 이미 CommunityInteractionsModule에서 NotificationModule을 import했는데,
    댓글 모듈(CommunityCommentsModule)엔 빠져있었다. NOTIFICATION_DISPATCH_PORT가
    DI 컨텍스트에서 해석되지 않아 앱 부팅이 실패했고(프로덕션 배포 헬스체크 실패,
    자동 롤백됨), 이번 커밋으로 정상 부팅되게 고친다.

--- src/api/service/community/comments/community-comments.module-definition.ts
+++ src/api/service/community/comments/community-comments.module-definition.ts
@@ -1,3 +1,4 @@
+import { NotificationModule } from '../../notification/notification.module';
 import { CommunitySharedModule } from '../shared/community-shared.module';
 import { CommunityPostCommentController } from '../controller/community-post-comment.controller';
 import { GetCommunityPostCommentsUseCase } from '../application/use-cases/get-community-post-comments.use-case';
@@ -9,7 +10,8 @@ import { COMMUNITY_COMMENT_READER_PORT } from '../application/ports/community-co
 import { COMMUNITY_COMMENT_WRITER_PORT } from '../application/ports/community-comment-writer.port';
 
 // 커뮤니티 > 댓글 슬라이스 (댓글·답글 작성/수정/삭제/조회)
-export const COMMUNITY_COMMENTS_MODULE_IMPORTS = [CommunitySharedModule];
+// 댓글·답글 작성 시 글쓴이·원댓글 작성자에게 알림을 보내므로 NotificationModule(DISPATCH Port)이 필요하다.
+export const COMMUNITY_COMMENTS_MODULE_IMPORTS = [CommunitySharedModule, NotificationModule];
 
 export const COMMUNITY_COMMENTS_MODULE_CONTROLLERS = [CommunityPostCommentController];
 
